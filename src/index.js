b074e05c6efb839920594e269c60b08cbc262d41
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-24 15:51:09 +0300

    Project Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const PORT = process.env.PORT || 3000;
+const express = require('express');
 
+const {configHbs} = require('./config/hbs');
+const { configExpress } = require('./config/express');
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+configHbs(app);
+configExpress(app);
 
 app.listen(PORT);
