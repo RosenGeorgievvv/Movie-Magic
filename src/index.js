6b25e7ca835e1d00e44c83cc6b7be6a04bb08d42
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-24 15:37:17 +0300

    project update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-const express = require('express');
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
 
 app.listen(PORT);
