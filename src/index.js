d974598ed289f9ebbb00e4f19bf7fc3293d6980b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-24 13:58:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 
+const PORT = 3000;
+
 const app = express();
 
 const hbs = handlebars.create({
@@ -13,4 +15,4 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded());
 app.use('/static', express.static('static'));
 
-app.listen(3000);
+app.listen(PORT);
