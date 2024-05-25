68b3463a45ab22e1e736da38887bceacd4c77a56
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-25 14:53:48 +0300

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const { home, details } = require("../controllers/catalog");
 const { about } = require("../controllers/about");
+const { create } = require("../controllers/movie");
 
 const router = Router();
 
 router.get('/', home);
 router.get('/about', about);
 router.get('/details/:id', details);
+router.get('/create', create);
 
 module.exports = { router };
