15452118f8fa5a5275be8b89b901c0be92516070
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-27 17:17:00 +0300

    Update movie.js

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,2 +1,14 @@
 const fs = require('fs/promises');
 
+const filePath = './data/database.json';
+
+async function readFile(){
+const data = await fs.readFile(filePath);
+
+return JSON.parse(data.toString());
+}
+
+async function writeFile(data){
+    await fs.writeFile(filePath, JSON.stringify(data));
+}
+
