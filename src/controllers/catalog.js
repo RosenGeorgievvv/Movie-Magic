5458e9f644dd3134cc66068162f5d68d02d2e6da
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-27 17:29:31 +0300

    Update

--- src/controllers/catalog.js
+++ src/controllers/catalog.js
@@ -1,14 +1,14 @@
 const { getAllMovies } = require("../services/movie");
 
 module.exports = {
-    home: async (req, res) =>{
-        const movies = await getAllMovies()
-        res.render('home', movies);
-    },
-    details: (req, res) =>{
-        res.render('details');
-    },
-    search: (req, res) =>{
-        res.render('search');
-    }
-}
+  home: async (req, res) => {
+    const movies = await getAllMovies();
+    res.render("home", { movies });
+  },
+  details: (req, res) => {
+    res.render("details");
+  },
+  search: (req, res) => {
+    res.render("search");
+  },
+};
