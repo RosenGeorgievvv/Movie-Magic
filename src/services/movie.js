70317ca782dea11c15665ce48e88985cbf11cf43
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-28 12:15:53 +0300

    Update movie.js

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,55 +1,59 @@
-const fs = require('fs/promises');
-const { Movie } = require('../models/Movie');
+const fs = require("fs/promises");
+const { Movie } = require("../models/Movie");
 
-const filePath = './data/database.json';
+const filePath = "./data/database.json";
 
-async function readFile(){
-const data = await fs.readFile(filePath);
+async function readFile() {
+  const data = await fs.readFile(filePath);
 
-return JSON.parse(data.toString());
+  return JSON.parse(data.toString());
 }
 
-async function writeFile(data){
-    await fs.writeFile(filePath, JSON.stringify(data));
+async function writeFile(data) {
+  await fs.writeFile(filePath, JSON.stringify(data));
 }
 
-function toMovieModel(data){
-    const movie = new Movie();
+function toMovieModel(data) {
+  const movie = new Movie();
 
-    movie.id = data.id;
-    movie.title = data.title;
-    movie.genre = data.genre;
-    movie.director = data.director;
-    movie.year = data.year;
-    movie.imageURL = data.imageURL;
-    movie.rating = data.rating;
-    movie.description = data.description;
+  movie.id = data.id;
+  movie.title = data.title;
+  movie.genre = data.genre;
+  movie.director = data.director;
+  movie.year = data.year;
+  movie.imageURL = data.imageURL;
+  movie.rating = data.rating;
+  movie.description = data.description;
 
-    return movie;
+  return movie;
 }
 
 async function getAllMovies() {
-    const movies = await readFile();
-    return movies.map(toMovieModel);
+  const movies = await readFile();
+  return movies.map(toMovieModel);
 }
 
-async function getMovieById(id){
-    const movies = await readFile();
+async function getMovieById(id) {
+  const movies = await readFile();
 
-    const movie = movies.find(m => m.id == id);
-    return movie ? toMovieModel(movie) : movie;
+  const movie = movies.find((m) => m.id == id);
+  return movie ? toMovieModel(movie) : movie;
 }
 
-async function createMovie(movieData){
-
-
+async function createMovie(movieData) {
+  const id = uuid();
+  const movie = {
+    
+  }
 }
 
-function uuid(){
-    return 'xxxx-xxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString(16));
+function uuid() {
+  return "xxxx-xxxx".replace(/x/g, () =>
+    ((Math.random() * 16) | 0).toString(16)
+  );
 }
 
 module.exports = {
-    getAllMovies,
-    getMovieById
-}
+  getAllMovies,
+  getMovieById,
+};
