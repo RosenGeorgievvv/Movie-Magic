4d8970e47ed47d9464fda8d176dc5f05a152462a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-31 14:41:57 +0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,4 +5,9 @@ const connectionString = 'mongodb://localhost:27017/movie-magic';
 async function configDatabase(){
 
     await mongoose.connect(connectionString);
+    console.log('Database connected');
 }
+
+module.exports ={
+    configDatabase
+};
