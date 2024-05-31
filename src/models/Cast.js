3b79bca7d6bb346edc127a71ba52a647aba3e336
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-31 15:16:57 +0300

    Update Cast.js

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,5 +1,26 @@
 const {Schema, SchemaTypes: Types, model} = require('mongoose');
 
 const castSchema = new Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 199
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
 })
