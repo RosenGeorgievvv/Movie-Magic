bb02e7f609592dadf688ec29969c44db57f6ff08
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-31 14:42:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const {configHbs} = require('./config/hbs');
-const { configExpress } = require('./config/express');
-const { router } = require('./config/routes')
+const express = require("express");
+const { configHbs } = require("./config/hbs");
+const { configExpress } = require("./config/express");
+const { router } = require("./config/routes");
+const { configDatabase } = require("./config/database");
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 
-configHbs(app);
-configExpress(app);
-app.use(router);
+async function start() {
+  const app = express();
 
-app.listen(PORT);
+  await configDatabase();
+
+  configHbs(app);
+
+  configExpress(app);
+  
+  app.use(router);
+
+  app.listen(PORT);
+}
