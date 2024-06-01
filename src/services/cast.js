4d59b588716b3ecb302c5a77de95c0b255d6cac1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-01 11:24:16 +0300

    Update cast.js

--- src/services/cast.js
+++ src/services/cast.js
@@ -2,10 +2,13 @@ const { Cast } = require('../models/Cast');
 
 async function createCast(castData){
 
-
 }
 
 async function getAllCast(){
 
-    
+
+}
+module.exports = {
+    createCast,
+    getAllCast
 }
