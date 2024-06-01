5cbbdab6536fae956e5b6cffbc1d336335c2b735
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-01 11:26:04 +0300

    Update cast.js

--- src/services/cast.js
+++ src/services/cast.js
@@ -1,7 +1,14 @@
 const { Cast } = require('../models/Cast');
 
 async function createCast(castData){
-
+ const cast = new Cast({
+    name: castData.name,
+    age: castData.age,
+    born: castData.born,
+    nameInMovie: castData.nameInMovie,
+    imageURL: castData.imageURL,
+    movie: castData.movie
+ })
 }
 
 async function getAllCast(){
