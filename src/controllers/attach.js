ed8889d0c270f883a173a2a2fe821a28aff225ac
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-02 15:28:59 +0300

    Update attach.js

--- src/controllers/attach.js
+++ src/controllers/attach.js
@@ -1,8 +1,18 @@
+const { getMovieById } = require("../services/movie");
+
 module.exports = {
     attachGet: async (req, res) =>{
+        const id = req.params.id;
+        const movie = await getMovieById(id);
 
+        if(!movie){
+            res.render('404');
+            return;
+        }
+        
+       res.render('cast-attach');
     },
     attachPost: async (req, res) =>{
-
+       
     }
 }
