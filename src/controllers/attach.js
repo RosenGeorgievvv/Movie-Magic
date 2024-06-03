4149c22cf7080fed24f9ed391888eef08777a14e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-03 10:58:28 +0300

    Update attach.js

--- src/controllers/attach.js
+++ src/controllers/attach.js
@@ -1,38 +1,38 @@
 const { getMovieById, attachCastToMovie } = require("../services/movie");
-const { getAllCast } = require('../services/cast');
+const { getAllCast } = require("../services/cast");
 
 module.exports = {
-    attachGet: async (req, res) =>{
-        const id = req.params.id;
-        const movie = await getMovieById(id);
+  attachGet: async (req, res) => {
+    const id = req.params.id;
+    const movie = await getMovieById(id);
 
-        if(!movie){
-            res.render('404');
-            return;
-        }
-       const allCast = await getAllCast(); 
-
-       res.render('cast-attach', {movie, allCast});
-    },
-    attachPost: async (req, res) =>{
-       const movieId = req.params.id;
-       const castId = req.body.cast;
-
-       if(!movieId || !castId){
-         res.status(400).end();
-         return;
-       }
-       const movie = await getMovieById(id);
-
-       if(castId == 'none'){
-        const allCast = await getAllCast();
-        res.render('cast-attach', {movie, allCast, error: true });
-
-        return;
-       }
-
-       await attachCastToMovie(movie, castId);
-
-       res.redirect('/details/' + movieId);
+    if (!movie) {
+      res.render("404");
+      return;
     }
-}
+    const allCast = await getAllCast();
+
+    res.render("cast-attach", { movie, allCast });
+  },
+  attachPost: async (req, res) => {
+    const movieId = req.params.id;
+    const castId = req.body.cast;
+
+    if (!movieId || !castId) {
+      res.status(400).end();
+      return;
+    }
+
+    if (castId == "none") {
+      const movie = await getMovieById(movieId);
+      const allCast = await getAllCast();
+      res.render("cast-attach", { movie, allCast, error: true });
+
+      return;
+    }
+    const movie = await getMovieById(movieId, true);
+    await attachCastToMovie(movie, castId);
+
+    res.redirect("/details/" + movieId);
+  },
+};
