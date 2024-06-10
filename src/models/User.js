9b8a0047fedaa7c32014f7e53c9d81e5a67bb4b6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-10 14:45:55 +0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,21 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-    email:{
-        type: String,
-        required: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-},
-{
-    collation: {local: 'en', strength: 2}
-});
+  },
+  {
+    collation: { local: "en", strength: 2 },
+  }
+);
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = { User };
