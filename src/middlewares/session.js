80c7f73749355f9908465a9dec0e22daf4b000b6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-10 16:16:18 +0300

    Update session.js

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -1,6 +1,17 @@
+const { verifyToken } = require("../services/token");
+
 function session(){
     return function(req, res, next){
+       const token = req.cookies.token;
 
+       if(token){
+        try {
+            const payload = verifyToken(token);
+            req.user = payload;
+        } catch (error) {
+            
+        }
+       }
     };
 }
 
