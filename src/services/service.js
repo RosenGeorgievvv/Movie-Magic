c3ad285157dee9039b58bc066013f832d4212fb3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-10 14:45:24 +0300

    Update service.js

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,20 @@
 const {User} = require('../models/User');
+const bcrypt = require('bcrypt');
 
 async function register(username, password){
 
+    const existing = await username.findOne({email});
+
+    if(existing){
+        throw new Error('Email is already used');
+    }
+    const user = new User({
+        email,
+        password: await bcrypt.hash(password, 10)
+    })
+
+   await user.save();
+   return user;
 }
 
 async function login(username, password){
