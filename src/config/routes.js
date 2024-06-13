01014ef71c49813f10ade35cc21f0ee1e2759b4b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-13 14:48:41 +0300

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -14,18 +14,21 @@ const router = Router();
 
 router.get('/', home);
 router.get('/about', about);
+router.get('/search', search);
+
 router.get('/details/:id', details);
 router.get('/attach/:id', isUser(), attachGet);
 router.post('/attach/:id', isUser(), attachPost);
-router.get('/create/movie', isUser(), createGet);
-router.post('/create/movie', isUser(), createPost);
 router.get('/edit/:id', isUser(), editGet);
 router.post('/edit/:id', isUser(), editPost);
 router.get('/delete/:id', isUser(), deleteGet);
 router.post('/delete/:id', isUser(), deletePost);
+
+router.get('/create/movie', isUser(), createGet);
+router.post('/create/movie', isUser(), createPost);
 router.get('/create/cast', isUser(), createCastGet);
 router.post('/create/cast', isUser(), createCastPost);
-router.get('/search', search);
+
 router.get('/register', isGuest(), registerGet);
 router.post('/register',isGuest(), registerPost);
 router.get('/login',isGuest(), loginGet);
